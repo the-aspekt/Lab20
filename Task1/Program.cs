e74e13f9f7f849d82787c8b674eb389adb02b2a9
Pavel <sozonovps@gmail.com>
2023-02-09 15:51:07 +0800

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = CalcCircumference;
+            Console.WriteLine(myDelegate(5.5));
+            myDelegate = CalcCircleArea;
+            Console.WriteLine(myDelegate(5.5));
+            myDelegate = CalcSphereVolume;
+            Console.WriteLine(myDelegate(5.5));
+            Console.ReadKey();
+        }
+
+        delegate double MyDelegate(double d);
+
+
+       static double CalcCircumference(double r) => r * 2 * Math.PI;
+
+        static double CalcCircleArea(double r) => r * r * Math.PI;
+
+        static double CalcSphereVolume(double r) => r * r *r* Math.PI*4/3;
+
+
+
+    }
+}
